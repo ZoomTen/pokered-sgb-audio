ab068bbe09a8d397423a6b0132c51c3482815014
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-10-22 22:58:11 +0700

    music_id_constants: add comment numbers to tracks

--- constants/music_id_constants.asm
+++ constants/music_id_constants.asm
@@ -1,55 +1,58 @@
 ; Music ID definitions
 	const_def
+
+; MSU-1 track numbers follow this, but add one to every constant
+
 ; beginning screens
-	const Mus_Intro
-	const Mus_TitleScreen
+	const Mus_Intro			; 0
+	const Mus_TitleScreen		; 1
 ; towns and cities
-	const Mus_Pallet
-	const Mus_Cities1
-	const Mus_Cities2
-	const Mus_Celadon
-	const Mus_Cinnabar
-	const Mus_Vermilion
-	const Mus_Lavender
-	const Mus_SSAnne
+	const Mus_Pallet		; 2
+	const Mus_Cities1		; 3 Viridian, Pewter
+	const Mus_Cities2		; 4 Cerulean, Fuschia
+	const Mus_Celadon		; 5
+	const Mus_Cinnabar		; 6
+	const Mus_Vermilion		; 7
+	const Mus_Lavender		; 8
+	const Mus_SSAnne		; 9
 ; routes
-	const Mus_Routes1
-	const Mus_Routes2
-	const Mus_Routes3
-	const Mus_Routes4
-	const Mus_IndigoPlateau
+	const Mus_Routes1		; 10 Routes 1, 2
+	const Mus_Routes2		; 11 Routes 24, 25
+	const Mus_Routes3		; 12 Routes 3-10, 16-22
+	const Mus_Routes4		; 13 Routes 11-15
+	const Mus_IndigoPlateau		; 14
 ; places
-	const Mus_Pokecenter
-	const Mus_Gym
-	const Mus_OaksLab
-	const Mus_GameCorner
-	const Mus_Dungeon1
-	const Mus_Dungeon2
-	const Mus_Dungeon3
-	const Mus_CinnabarMansion
-	const Mus_PokemonTower
-	const Mus_SilphCo
+	const Mus_Pokecenter		; 15
+	const Mus_Gym			; 16
+	const Mus_OaksLab		; 17
+	const Mus_GameCorner		; 18
+	const Mus_Dungeon1		; 19 Rocket Hideout
+	const Mus_Dungeon2		; 20 Viridian Forest
+	const Mus_Dungeon3		; 21 Mt. Moon
+	const Mus_CinnabarMansion	; 22
+	const Mus_PokemonTower		; 23
+	const Mus_SilphCo		; 24
 ; encounters
-	const Mus_MeetProfOak
-	const Mus_MeetRival
-	const Mus_MeetEvilTrainer
-	const Mus_MeetFemaleTrainer
-	const Mus_MeetMaleTrainer
+	const Mus_MeetProfOak		; 25
+	const Mus_MeetRival		; 26
+	const Mus_MeetEvilTrainer	; 27
+	const Mus_MeetFemaleTrainer	; 28
+	const Mus_MeetMaleTrainer	; 29
 ; events
-	const Mus_FollowMe
-	const Mus_Evolution
-	const Mus_BikeRiding
-	const Mus_Surfing
-	const Mus_Jigglypuff
+	const Mus_FollowMe		; 30
+	const Mus_Evolution		; 31
+	const Mus_BikeRiding		; 32
+	const Mus_Surfing		; 33
+	const Mus_Jigglypuff		; 34
 ; battles
-	const Mus_WildBattle
-	const Mus_TrainerBattle
-	const Mus_GymLeaderBattle
-	const Mus_ChampionBattle
+	const Mus_WildBattle		; 35
+	const Mus_TrainerBattle		; 36
+	const Mus_GymLeaderBattle	; 37
+	const Mus_ChampionBattle	; 38
 ; victories
-	const Mus_DefeatedWildMon
-	const Mus_DefeatedTrainer
-	const Mus_DefeatedGymLeader
+	const Mus_DefeatedWildMon	; 39
+	const Mus_DefeatedTrainer	; 40
+	const Mus_DefeatedGymLeader	; 41
 ; end
-	const Mus_HallOfFame
-	const Mus_Credits
+	const Mus_HallOfFame		; 42
+	const Mus_Credits		; 43
